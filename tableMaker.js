50d53da4f026518fa3e3530d61648505ddf422e3
Caleb T. Vanorio <caleb.vanorio@centre.edu>
2019-02-28 15:14:10 -0500

    hope

--- tableMaker.js
+++ tableMaker.js
@@ -1,29 +1,25 @@
+
+
 var makeTable() = function(){
-  people = [peopleMaker("Eileen","CSC","Sophmore","Female"),peopleMaker("Caleb","CSC","Senior","Male")
-  ,peopleMaker("Michael","CSC","10 years removed","Male")
-    ,peopleMaker("Emma","Math","Sophmore","Female"),peopleMaker("Rob","CSC","Sophmore","Male")];
+  people = [peopleMaker("Eileen","CSC","Sophmore","Female"),peopleMaker("Caleb","CSC","Senior","Male"),
+  peopleMaker("Michael","CSC","10 years removed","Male"),
+  peopleMaker("Emma","Math","Sophmore","Female"),peopleMaker("Rob","CSC","Sophmore","Male")];
 
   var table = d3.select(".starter").append("table");
   var rows = d3.selectAll("tr")
                .data(people)
                .enter()
                .append("tr");
+               
   rows.append("td")
-      .text(function(d){
-        return d.name
-      });
+      .text(function(d){return d.name});
   rows.append("td")
-      .text(function(d){
-        return d.major
-      });
+      .text(function(d){return d.major});
   rows.append("td")
-      .text(function(d){
-        return d.year
-      });
+      .text(function(d){return d.year});
+
   rows.append("td")
-      .text(function(d){
-        return d.gender
-      });
+      .text(function(d){return d.gender});
 };
 
 var peopleMaker = function(name, major, year, gender){
