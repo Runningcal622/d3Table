798eb60d241a2c10fe1328e0745e4314f5251cd4
Caleb T. Vanorio <caleb.vanorio@centre.edu>
2019-02-28 15:03:57 -0500

    no

--- tableMaker.js
+++ tableMaker.js
@@ -1,5 +1,4 @@
-var makeTable() = function()
-{
+var makeTable() = function(){
   people = [peopleMaker("Eileen","CSC","Sophmore","Female"),peopleMaker("Caleb","CSC","Senior","Male")
   ,peopleMaker("Michael","CSC","10 years removed","Male")
     ,peopleMaker("Emma","Math","Sophmore","Female"),peopleMaker("Rob","CSC","Sophmore","Male")];
